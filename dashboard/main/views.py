e01ef1a1a7721099e15eb46e4c684d0a743aa790
GuevaraMarius <irabizimguevara@gmail.com>
2025-06-26 17:47:16 +0200

    Feat: postgres integration

--- dashboard/main/views.py
+++ dashboard/main/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render 
 from django.http import HttpResponse
+from .models import DashboardStat, Transaction 
 
 
 def index(request):
-    stats = {
-        'users': 1234,
-        'revenue': 9876,
-        'errors': 3,
-        'session': 6
+    stats = DashboardStat.objects.latest('created_at')
+    transactions = Transaction.objects.order_by('-created_at')[:10]
+
+    context = {
+        'users': stats.users,
+        'revenue': stats.revenue,
+        'errors': stats.errors,
+        'session': stats.session,
+        'transactions': transactions
     }
-    return render(request, 'main/index.html', stats)
+    return render(request, 'main/index.html', context)
 
 def home(request):
     return HttpResponse("This is my home page")
